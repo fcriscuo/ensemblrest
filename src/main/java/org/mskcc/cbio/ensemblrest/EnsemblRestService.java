4caa9bf11cacc9fe157c24e63f67cb866361a9c7
Fred Criscuolo <fjcriscuolo@ucsd.edu>
2015-08-22 14:16:35 -0400

    Initial commit

---
+++ src/main/java/org/mskcc/cbio/ensemblrest/EnsemblRestService.java
@@ -0,0 +1,96 @@
+package org.mskcc.cbio.ensemblrest;
+
+import com.google.api.client.repackaged.com.google.common.base.Preconditions;
+import com.google.api.client.repackaged.com.google.common.base.Strings;
+import org.apache.log4j.Logger;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Copyright (c) 2015 Memorial Sloan-Kettering Cancer Center.
+ * <p>
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ * documentation provided hereunder is on an "as is" basis, and
+ * Memorial Sloan-Kettering Cancer Center
+ * has no obligations to provide maintenance, support,
+ * updates, enhancements or modifications.  In no event shall
+ * Memorial Sloan-Kettering Cancer Center
+ * be liable to any party for direct, indirect, special,
+ * incidental or consequential damages, including lost profits, arising
+ * out of the use of this software and its documentation, even if
+ * Memorial Sloan-Kettering Cancer Center
+ * has been advised of the possibility of such damage.
+ * <p>
+ * Created by fcriscuo on 4/11/15.
+ */
+public class EnsemblRestService {
+
+    private static final Logger logger = Logger.getLogger(EnsemblRestService.class);
+    private static final String ENSEMBL_URL_TEMPLATE = "http://grch37.rest.ensembl.org/vep/human/hgvs/GENPOS?";
+
+    public EnsemblRestService() {}
+/*
+public method to retrieve ensembl VEP annotations for a specified variant
+returns resultS IN JSON FORMAT
+ */
+    public String resolveVEPByGenomicLocation (String genomicLocation){
+        Preconditions.checkArgument(!Strings.isNullOrEmpty(genomicLocation),
+                "A genomic location in HGVS notation is required");
+        try {
+            URL url = new URL(ENSEMBL_URL_TEMPLATE.replace("GENPOS",genomicLocation));
+            URLConnection connection = url.openConnection();
+            HttpURLConnection httpConnection = (HttpURLConnection) connection;
+            httpConnection.setRequestProperty("Content-Type", "application/json");
+            InputStream response = connection.getInputStream();
+            int responseCode = httpConnection.getResponseCode();
+            if(responseCode != 200) {
+                if(responseCode == 429 && httpConnection.getHeaderField("Retry-After") != null) {
+                    double sleepFloatingPoint = Double.valueOf(httpConnection.getHeaderField("Retry-After"));
+                    double sleepMillis = 1000 * sleepFloatingPoint;
+                    Thread.sleep((long)sleepMillis);
+                    return resolveVEPByGenomicLocation(genomicLocation);
+                }
+                logger.error("Response code was not 200. Detected response was " + responseCode +
+                        " for genomic location: " + genomicLocation);
+            }
+
+            try(Reader reader  = new BufferedReader(new InputStreamReader(response, "UTF-8"))) {
+                StringBuilder builder = new StringBuilder();
+                char[] buffer = new char[8192];
+                int read;
+                while ((read = reader.read(buffer, 0, buffer.length)) > 0) {
+                    builder.append(buffer, 0, read);
+                }
+                return builder.toString();
+
+            } catch (IOException ex) {
+                    logger.error(ex.getMessage());
+            }
+
+        } catch ( InterruptedException |IOException e) {
+            logger.error(e.getMessage());
+            e.printStackTrace();
+        }
+        return "";
+
+    }
+
+    /*
+    main method for stand alone testing
+     */
+    public static void main(String[] args) throws Exception {
+        EnsemblRestService service = new EnsemblRestService();
+        String genomicLocation1 = "17:g.41270062G>A";
+        System.out.println(service.resolveVEPByGenomicLocation(genomicLocation1));
+        String genomicLocation2 = "10:g.42936755A>C";
+        System.out.println(service.resolveVEPByGenomicLocation(genomicLocation2));
+        String genomicLocation3 = "7:g.140453999G>T";
+        System.out.println(service.resolveVEPByGenomicLocation(genomicLocation3));
+    }
+}
---
+++ src/main/java/org/mskcc/cbio/vep/data/VepAnnotator.java
@@ -0,0 +1,100 @@
+package org.mskcc.cbio.vep.data;
+
+import com.google.api.client.repackaged.com.google.common.base.Preconditions;
+import com.google.api.client.repackaged.com.google.common.base.Strings;
+import com.google.common.base.Optional;
+import org.apache.log4j.Logger;
+import org.mskcc.cbio.ensemblrest.EnsemblRestService;
+import org.mskcc.cbio.vep.database.dao.VepJsonCacheMapper;
+import org.mskcc.cbio.vep.database.model.VepJsonCache;
+import org.mskcc.cbio.vep.database.model.VepJsonCacheExample;
+import org.mskcc.cbio.vep.util.VEPSessionManager;
+
+import java.util.List;
+
+/**
+ * Copyright (c) 2015 Memorial Sloan-Kettering Cancer Center.
+ * <p/>
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ * documentation provided hereunder is on an "as is" basis, and
+ * Memorial Sloan-Kettering Cancer Center
+ * has no obligations to provide maintenance, support,
+ * updates, enhancements or modifications.  In no event shall
+ * Memorial Sloan-Kettering Cancer Center
+ * be liable to any party for direct, indirect, special,
+ * incidental or consequential damages, including lost profits, arising
+ * out of the use of this software and its documentation, even if
+ * Memorial Sloan-Kettering Cancer Center
+ * has been advised of the possibility of such damage.
+ * <p/>
+ * Created by Fred Criscuolo on 4/12/15.
+ * criscuof@mskcc.org
+ */
+public class VepAnnotator {
+    private static final Logger logger = Logger.getLogger(VepAnnotator.class);
+    private final EnsemblRestService service = new EnsemblRestService();
+    private final VepJsonCacheMapper cacheMapper;
+    private final VepJsonCacheExample cacheExample;
+
+
+    public VepAnnotator() {
+
+        this.cacheMapper = VEPSessionManager.INSTANCE.getVepSession().getMapper(VepJsonCacheMapper.class);
+        this.cacheExample = new VepJsonCacheExample();
+    }
+
+    public String annotateVariation(String variation) {
+        Preconditions.checkArgument(!Strings.isNullOrEmpty(variation),
+                "A variation in HGVS notation is required");
+        // check database
+        Optional<String> annotationOpt = this.findAnnotationInDatabase(variation);
+        if (annotationOpt.isPresent()) {
+            logger.info("====> variation " + variation + " found in database");
+            return annotationOpt.get();
+        }
+        // get from ensembl REST service
+        String annotation = this.service.resolveVEPByGenomicLocation(variation);
+        if (!Strings.isNullOrEmpty(annotation)) {
+            this.insertAnnotationIntoDatabase(variation, annotation);
+        }
+        return annotation;
+    }
+
+    private void insertAnnotationIntoDatabase(String variation, String json){
+        if(Strings.isNullOrEmpty(json)){
+            logger.info("No annotation for " +variation);
+        }
+        VepJsonCache jsonCache = new VepJsonCache();
+        jsonCache.setCACHE_KEY(variation);
+        jsonCache.setRAW_JSON(json);
+        this.cacheMapper.insert(jsonCache);
+        VEPSessionManager.INSTANCE.getVepSession().commit();
+        logger.info("====> variation " +variation +" inserted into database");
+    }
+        private Optional<String> findAnnotationInDatabase(String variation){
+        this.cacheExample.clear();
+        this.cacheExample.createCriteria().andCACHE_KEYEqualTo(variation);
+        List<VepJsonCache> vepList =this.cacheMapper.selectByExampleWithBLOBs(this.cacheExample);
+        if (vepList.isEmpty()) { return Optional.absent();}
+        if(vepList.size() > 1) {
+            logger.error("variation: " +variation +" is not unique");
+        }
+            if ( null != vepList.get(0).getRAW_JSON() ) {
+                return Optional.of(vepList.get(0).getRAW_JSON());
+            }
+            this.deleteRow(variation);
+            return Optional.absent();
+    }
+
+    private void deleteRow(String variation){
+        this.cacheExample.clear();
+        this.cacheExample.createCriteria().andCACHE_KEYEqualTo(variation);
+        this.cacheMapper.deleteByExample(cacheExample);
+        VEPSessionManager.INSTANCE.getVepSession().commit();
+        logger.info("===>Deleted row with variation " +variation);
+    }
+
+
+}

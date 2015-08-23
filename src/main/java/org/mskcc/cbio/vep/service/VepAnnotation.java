d4be161276ca9ee9eea19540a878aa8c2daa2ec2
Fred Criscuolo <fjcriscuolo@ucsd.edu>
2015-08-23 17:31:10 -0400

    add VepAnnotation class to commit

--- src/main/java/org/mskcc/cbio/vep/service/VepAnnotation.java
+++ src/main/java/org/mskcc/cbio/vep/service/VepAnnotation.java
@@ -1,7 +1,25 @@
 package org.mskcc.cbio.vep.service;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * Created by fcriscuo on 8/22/15.
  */
 public class VepAnnotation {
+    private String variation;
+    private String annotation;
+
+    public VepAnnotation(){}
+
+    public VepAnnotation(String variation, String annotation){
+        this.variation = variation;
+        this.annotation = annotation;
+    }
+
+    @JsonProperty
+    public String getVariation () { return this.variation;}
+
+    @JsonProperty
+    public String getAnnotation() { return this.annotation;}
+
 }
